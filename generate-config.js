442133e37951f85a1b7c66ef7de070b6dd0af04b
Nebojsa1999 <nebojsa253@gmail.com>
2026-03-28 18:41:38 +0100

    commit

--- generate-config.js
+++ generate-config.js
@@ -4,5 +4,6 @@ const config = {
     apiUrl: process.env.API_URL || 'http://localhost:8081'
 };
 
+fs.mkdirSync('./src/config', { recursive: true });
 fs.writeFileSync('./src/config/config.json', JSON.stringify(config, null, 2));
 console.log('Config generated:', config);
